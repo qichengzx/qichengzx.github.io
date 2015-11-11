f323676cc85472de480f69d424c6d35ed42f4da2
启程 <qichengzx@gmail.com>
2015-11-11 14:01:19 +0800

    添加一个时钟的挂件

--- themes/landscape/source/js/script.js
+++ themes/landscape/source/js/script.js
@@ -134,4 +134,27 @@
 
     $container.removeClass('mobile-nav-on');
   });
+
+  var now = new Date();
+  var second = now.getSeconds();
+  var minute = now.getMinutes();
+  var hour = now.getHours();
+  if (hour > 12) {
+      hour = hour - 12;
+  }
+  hourDeg   = hour * 30 + now.getMinutes() / 60 * 30;
+  minuteDeg = now.getMinutes() * 6;
+  secondDeg = now.getSeconds() * 6;
+  stylesDeg = [
+      "@keyframes rotate-hour{ from{transform:rotate(" + hourDeg + "deg);}to{transform:rotate(" + (hourDeg + 360) + "deg);}}",
+      "@keyframes rotate-minute{from{transform:rotate(" + minuteDeg + "deg);}to{transform:rotate(" + (minuteDeg + 360) + "deg);}}",
+      "@keyframes rotate-second{from{transform:rotate(" + secondDeg + "deg);}to{transform:rotate(" + (secondDeg + 360) + "deg);}}",
+      "@-moz-keyframes rotate-hour{ from{transform:rotate(" + hourDeg + "deg);}to{transform:rotate(" + (hourDeg + 360) + "deg);}}",
+      "@-moz-keyframes rotate-minute{from{transform:rotate(" + minuteDeg + "deg);}to{transform:rotate(" + (minuteDeg + 360) + "deg);}}",
+      "@-moz-keyframes rotate-second{from{transform:rotate(" + secondDeg + "deg);}to{transform:rotate(" + (secondDeg + 360) + "deg);}}",
+      "@-webkit-keyframes rotate-hour{from{transform:rotate(" + hourDeg + "deg);}to{transform:rotate(" + (hourDeg + 360) + "deg);}}",
+      "@-webkit-keyframes rotate-minute{from{transform:rotate(" + minuteDeg + "deg);}to{transform:rotate(" + (minuteDeg + 360) + "deg);}}",
+      "@-webkit-keyframes rotate-second{from{transform:rotate(" + secondDeg + "deg);}to{transform:rotate(" + (secondDeg + 360) + "deg);}}"
+  ].join("");
+  $('#clock-animations').html(stylesDeg);
 })(jQuery);
